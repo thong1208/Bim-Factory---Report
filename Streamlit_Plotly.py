0391e1c80f13d14fdbb92669c7efad623b424fdb
thong1208 <thongdoan1208@gmail.com>
2022-08-07 11:14:53 +0700

    Update

--- Streamlit_Plotly.py
+++ Streamlit_Plotly.py
@@ -4,7 +4,6 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 from  plotly.subplots  import  make_subplots 
-from user_agents import parse
 import user_agents
 
 # layout='wide'
